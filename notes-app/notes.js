7538de97ba7b01c4180e69c422004825a25e8930
nacho <nacho.conso@gmail.com>
2020-09-09 09:37:53 -0300

    added add and remove functionalities

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,52 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
 const getNotes = function () {
-  return "your notes";
+  return "your notes...";
 };
 
-module.exports = getNotes;
+const addNote = function (title, body) {
+  const notes = loadNotes();
+  const notesDuplicate = notes.filter((note) => note.title === title);
+  if (notesDuplicate.length === 0) {
+    notes.push({ title: title, body: body });
+    saveNotes(notes);
+    console.log(chalk.green.inverse("New note added!"));
+  } else {
+    console.log(chalk.red.inverse("Note title already taken!"));
+  }
+};
+
+const removeNote = function (title) {
+  const notes = loadNotes();
+  const updatedNotes = notes.filter((note) => note.title !== title);
+  if (updatedNotes.length < notes.length) {
+    console.log(chalk.green.inverse("Note removed"));
+    saveNotes(updatedNotes);
+  } else {
+    console.log(chalk.red.inverse("Note not found"));
+  }
+};
+
+const saveNotes = function (notes) {
+  const notesJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", notesJSON);
+};
+
+const loadNotes = function (notes) {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    const notes = JSON.parse(dataJSON);
+    return notes;
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
+};
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote,
+  removeNote: removeNote,
+};
